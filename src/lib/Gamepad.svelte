ce4501bd0075c3412ebea12c31bd5ad4c321e963
Simón Negrín <simon.negrin@gmail.com>
2025-07-06 21:56:40 +0100

    Portrait device warning

--- src/lib/Gamepad.svelte
+++ src/lib/Gamepad.svelte
@@ -1,21 +1,40 @@
 <script lang="ts">
+  import { fade } from 'svelte/transition'
   import type { Point } from '../vite-env'
   import Joystick from './Joystick.svelte'
+
+  let showPortraitWarn = $state(isDeviceVertical())
 
   function onJoystickInput(point: Point): void {
     console.log(point)
   }
 
+  function onorientationchange(): void {
+    showPortraitWarn = isDeviceVertical()
+  }
+
+  function isDeviceVertical(): boolean {
+    return screen.orientation.type.startsWith('portrait')
+  }
+
 </script>
 <main>
+  {#if showPortraitWarn}
+    <div class="portrait-warn" transition:fade>
+      <div>Coloca el dispositivo en horizontal</div>
+    </div>
+  {/if}
+
   <div class="ctrl joystick-wrapper">
     <Joystick oninput={onJoystickInput} />
   </div>
-  <div class="ctrl btn btn-a"></div>
-  <div class="ctrl btn btn-b"></div>
-  <div class="ctrl btn btn-c"></div>
+  <div class="ctrl trigger trigger-a"></div>
+  <div class="ctrl trigger trigger-b"></div>
+  <div class="ctrl trigger trigger-c"></div>
   <div class="ctrl btn-start"></div>
 </main>
+
+<svelte:window onorientationchange={onorientationchange} />
 
 <style>
   main {
@@ -29,9 +48,25 @@
     background-position: -20px center;
     background-repeat: no-repeat;
   }
+  .portrait-warn {
+    position: absolute;
+    z-index: 10;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: red;
+    color: #fff;
+    font-size: 2rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 2rem;
+    text-align: center;
+  }
   .ctrl {
     position: absolute;
-    background-color: rgba(248, 255, 157, 0.405);
+    /* background-color: rgba(248, 255, 157, 0.405); */
   }
   .joystick-wrapper {
     left: 5%;
@@ -40,19 +75,19 @@
     aspect-ratio: 1;
 
   }
-  .btn {
+  .trigger {
     width: 10%;
     aspect-ratio: 1;
   }
-  .btn-a {
+  .trigger-a {
     top: 49%;
     left: 63%;
   }
-  .btn-b {
+  .trigger-b {
     top: 39%;
     left: 74%;
   }
-  .btn-c {
+  .trigger-c {
     top: 31%;
     left: 85%;
   }
