02b9f791e654101e1b3d3e52bfdf2df98df3ab6e
Simón Negrín <simon.negrin@gmail.com>
2025-07-06 20:39:21 +0100

    Init project

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<main>
+  
+</main>
+
+<style>
+  main {
+    width: 100vw;
+    height: 100vh;
+    background-color: rgb(20, 20, 20);
+    background-image: url('/megadrive_gamepad.png');
+    background-size: calc(100% + 40px) auto;
+    background-position: -20px center;
+    background-repeat: no-repeat;
+  }
+</style>
